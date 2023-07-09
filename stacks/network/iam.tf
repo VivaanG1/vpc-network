f8644b9c068151a875df56f1d65b08fca00b978b
mustard2890 <mustard2890@corp.amazonworkspaces.com>
2023-07-09 11:37:53 +0100

    updated flowlogs

---
+++ stacks/network/iam.tf
@@ -0,0 +1,51 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_iam_role" "flow_log_role" {
+  name               = "${var.environment}-sdp-flow-log-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "vpc-flow-logs.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "cloudwatch_logs_policy" {
+  name        = "${var.environment}-sdp-flow-log-policy"
+  description = "Provides full access to CloudWatch Logs"
+  policy      = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogGroups",
+        "logs:DescribeLogStreams"
+      ],
+      "Resource": "${aws_cloudwatch_log_group.flow_log_group.arn}:*"
+    }
+  ]
+}
+EOF
+}
+
+# Attach the policy to the IAM role
+resource "aws_iam_role_policy_attachment" "flow_log_policy_attachment" {
+  role       = aws_iam_role.my_flow_log_role.name
+  policy_arn = aws_iam_policy.cloudwatch_logs_policy.arn
+}
