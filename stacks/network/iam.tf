9e81a8cdc667463097e59ef06f205ffa805decfb
mustard2890 <mustard2890@corp.amazonworkspaces.com>
2023-07-09 11:41:13 +0100

    updated flowlogs

--- stacks/network/iam.tf
+++ stacks/network/iam.tf
@@ -1,5 +1,3 @@
-data "aws_caller_identity" "current" {}
-
 resource "aws_iam_role" "flow_log_role" {
   name               = "${var.environment}-sdp-flow-log-role"
   assume_role_policy = <<EOF
@@ -46,6 +44,6 @@ EOF
 
 # Attach the policy to the IAM role
 resource "aws_iam_role_policy_attachment" "flow_log_policy_attachment" {
-  role       = aws_iam_role.my_flow_log_role.name
+  role       = aws_iam_role.flow_log_role.name
   policy_arn = aws_iam_policy.cloudwatch_logs_policy.arn
 }
