159be63bbcefb8710b211fbbb12bf4ac23fae807
mustard2890 <mustard2890@corp.amazonworkspaces.com>
2023-07-11 00:29:11 +0100

    test

--- stacks/network/main.tf
+++ stacks/network/main.tf
@@ -15,85 +15,90 @@ resource "aws_internet_gateway" "igw" {
 }
 
 variable "availability_zones" {
-  default = ["eu-west-1a", "eu-west-1b", "eu-west-1c"] # Update with your desired AZs
+  default = ["eu-west-1a", "eu-west-1b"] # Update with your desired AZs
 }
 
 resource "aws_subnet" "public_subnet" {
-  count             = 3
+  count             = 2
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = "10.0.${count.index}.0/24" # Adjust the CIDR block as needed
   availability_zone = var.availability_zones[count.index]
 
   tags = {
-    Name = "${var.environment}-sdp-PublicSubnet-${count.index}"
+    Name = "${var.environment}-sdp-PublicSubnet-${count.index + 1}"
   }
 }
 
 resource "aws_subnet" "private_subnet" {
-  count             = 3
+  count             = 2
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = "10.0.${count.index + 10}.0/24" # Adjust the CIDR block as needed
   availability_zone = var.availability_zones[count.index]
 
   tags = {
-    Name = "${var.environment}-sdp-PrivateSubnet-${count.index}"
+    Name = "${var.environment}-sdp-PrivateSubnet-${count.index + 1}"
   }
 }
 
 resource "aws_nat_gateway" "nat_gateway" {
-  allocation_id = aws_eip.eip.id
-  subnet_id     = aws_subnet.public_subnet[0].id
+  count         = 2
+  allocation_id = aws_eip.nat_eip[count.index].id
+  subnet_id     = aws_subnet.public_subnet[count.index].id
 
   tags = {
-    Name = "${var.environment}-sdp-ngw"
+    Name = "${var.environment}-sdp-nat-gateway-${count.index + 1}"
   }
 }
 
-resource "aws_eip" "eip" {
-  vpc = true
+resource "aws_eip" "nat_eip" {
+  count = 2
+  vpc   = true
 
   tags = {
-    Name = "${var.environment}-sdp-eip"
+    Name = "${var.environment}-sdp-eip-${count.index + 1}"
   }
 }
 
+# Create Public Route Tables
 resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
+  count    = 2
+  vpc_id   = aws_vpc.my_vpc.id
 
   tags = {
-    Name = "${var.environment}-sdp-public-rt"
+    Name = "PublicRouteTable-${count.index + 1}"
   }
 }
 
+# Create Private Route Tables
 resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.vpc.id
+  count    = 2
+  vpc_id   = aws_vpc.my_vpc.id
 
   tags = {
-    Name = "${var.environment}-sdp-private-rt"
+    Name = "PrivateRouteTable-${count.index + 1}"
   }
 }
 
+# Associate Public Subnets with Public Route Tables
 resource "aws_route_table_association" "public_subnet_association" {
-  count          = 3
+  count          = 2
   subnet_id      = aws_subnet.public_subnet[count.index].id
-  route_table_id = aws_route_table.public_route_table.id
+  route_table_id = aws_route_table.public_route_table[count.index].id
 }
 
+# Associate Private Subnets with Private Route Tables
 resource "aws_route_table_association" "private_subnet_association" {
-  count          = 3
+  count          = 2
   subnet_id      = aws_subnet.private_subnet[count.index].id
-  route_table_id = aws_route_table.private_route_table.id
+  route_table_id = aws_route_table.private_route_table[count.index].id
 }
 
+# Create Default Routes in Private Route Tables via NAT Gateways
 resource "aws_route" "private_default_route" {
-  route_table_id         = aws_route_table.private_route_table.id
+  count                 = 2
+  route_table_id        = aws_route_table.private_route_table[count.index].id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat_gateway.id
+  nat_gateway_id        = aws_nat_gateway.nat_gateway[count.index].id
 }
 
 resource "aws_cloudwatch_log_group" "flow_log_group" {
