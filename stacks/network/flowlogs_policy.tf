05ab9a25e41c9cb7cb09035d715957efd7a99a87
mustard <mustard@corp.amazonworkspaces.com>
2023-07-26 16:15:39 +0100

    test11

--- stacks/network/flowlogs_policy.tf
+++ stacks/network/flowlogs_policy.tf
@@ -1,46 +1,48 @@
-resource "aws_iam_role" "flow_log_role" {
-  name               = "${var.environment}-sdp-flow-log-role"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "vpc-flow-logs.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
+data "aws_iam_policy_document" "flow_log_assume_role_policy" {
+  version = "2012-10-17"
+
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["vpc-flow-logs.amazonaws.com"]
     }
-  ]
+  }
 }
-EOF
+
+data "aws_iam_policy_document" "cloudwatch_logs_policy" {
+  version = "2012-10-17"
+
+  statement {
+    actions   = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:DescribeLogGroups",
+      "logs:DescribeLogStreams",
+    ]
+    effect = "Allow"
+
+    resources = [
+      "${aws_cloudwatch_log_group.flow_log_group.arn}:*",
+    ]
+  }
+}
+
+resource "aws_iam_role" "flow_log_role" {
+  name = "${var.environment}-sdp-flow-log-role"
+  assume_role_policy = data.aws_iam_policy_document.flow_log_assume_role_policy.json
 }
 
 resource "aws_iam_policy" "cloudwatch_logs_policy" {
   name        = "${var.environment}-sdp-flow-log-policy"
   description = "Provides full access to CloudWatch Logs"
-  policy      = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents",
-        "logs:DescribeLogGroups",
-        "logs:DescribeLogStreams"
-      ],
-      "Resource": "${aws_cloudwatch_log_group.flow_log_group.arn}:*"
-    }
-  ]
-}
-EOF
+  policy = data.aws_iam_policy_document.cloudwatch_logs_policy.json
 }
 
-# Attach the policy to the IAM role
-resource "aws_iam_role_policy_attachment" "flow_log_policy_attachment" {
-  role       = aws_iam_role.flow_log_role.name
+resource "aws_iam_policy_attachment" "flow_log_policy_attachment" {
+  role       = [aws_iam_role.flow_log_role.name]
   policy_arn = aws_iam_policy.cloudwatch_logs_policy.arn
+  
 }
