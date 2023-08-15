4e4be4d1ee570426bd98f5d0940f2e2c0ae8d1eb
Vivaan <ksubodhgulia@gmail.com>
2023-08-15 20:14:20 +0100

    update

--- stacks/runner/variables.tf
+++ stacks/runner/variables.tf
@@ -2,12 +2,6 @@ variable "repo_url" {
   description = "Value of the REPO_URL"
   type        = string
 }
-
-variable "repo_url" {
-  description = "Value of the REPO_URL"
-  type        = string
-}
-
 variable "github_runner_token" {
   description = "Value of the github_runner_token"
   type        = string
