eb2009f48d3a510d9f919766aea3c736d9cfb707
Vivaan <ksubodhgulia@gmail.com>
2023-08-15 21:35:52 +0100

    update

--- stacks/runner/main.tf
+++ stacks/runner/main.tf
@@ -9,6 +9,12 @@ resource "aws_security_group" "runner_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "runner_instance" {
--- stacks/runner/userdata.sh
+++ stacks/runner/userdata.sh
@@ -2,8 +2,8 @@
 sudo yum update -y
 sudo yum install libicu -y
 mkdir actions-runner && cd actions-runner
-curl -O -L https://github.com/actions/runner/releases/download/v2.308.0/actions-runner-linux-x64-2.308.0.tar.gz
-tar xzf ./actions-runner-linux-x64-2.308.0.tar.gz
+curl -o actions-runner-linux-arm64-2.308.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.308.0/actions-runner-linux-arm64-2.308.0.tar.gz
+tar xzf ./actions-runner-linux-arm64-2.308.0.tar.gz
 ./config.sh --url "${repo_url}" --token "${github_runner_token}" --name "${runner_name}" --labels "${runner_name}" --unattended
 sudo ./svc.sh install
 sudo ./svc.sh start
