02e25322184678b7827e00496e90bf3a721ad579
Vivaan <ksubodhgulia@gmail.com>
2023-08-22 11:36:33 +0100

    update

--- stacks/runner/main.tf
+++ stacks/runner/main.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "github_runner_instance" {
   instance_type = "t4g.small"
   subnet_id     = "subnet-0140c6f3cbd5d56c7"
   key_name      = "newacct01"
-  user_data     = file("path/to/userdata.sh")
+  user_data     = file("./stacks/runner/userdata.sh")
 
   tags = {
     Name = each.key
