b73d75e9bb0c325d9d42974a28dc89d95c1bfeb1
Vivaan <ksubodhgulia@gmail.com>
2023-08-22 11:22:19 +0100

    tst

--- stacks/runner/main.tf
+++ stacks/runner/main.tf
@@ -1,31 +1,14 @@
-resource "aws_security_group" "runner_sg" {
-  name        = "sdp-runner-SG"
-  description = "sdp-runner-SG"
-  vpc_id      = "vpc-0187b32a2d48cadd4"
+resource "aws_instance" "github_runner_instance" {
+  for_each = var.github_repos
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_instance" "runner_instance" {
   ami                    = "ami-02df7c0eecb64fb85"
   instance_type          = "t4g.small"
   subnet_id              = "subnet-0140c6f3cbd5d56c7"
-  vpc_security_group_ids = [aws_security_group.runner_sg.id]
+  vpc_security_group_ids = "sg-0189f66c88b7f3708"
   key_name               = "newacct01"
-  user_data              = file("./stacks/runner/userdata.sh")
+  user_data              = file("path/to/userdata.sh")
 
   tags = {
-    Name = var.runner_name
+    Name = each.key
   }
 }
--- stacks/runner/userdata.sh
+++ stacks/runner/userdata.sh
@@ -5,6 +5,6 @@ cd /home/ec2-user
 mkdir actions-runner && cd actions-runner
 curl -o actions-runner-linux-arm64-2.308.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.308.0/actions-runner-linux-arm64-2.308.0.tar.gz
 tar xzf ./actions-runner-linux-arm64-2.308.0.tar.gz
-./config.sh --url "${repo_url}" --token "${github_runner_token}" --name "${runner_name}" --labels "${runner_name}" --unattended
+./config.sh --url each.key --token each.value --name each.key --unattended
 sudo ./svc.sh install
 sudo ./svc.sh start
