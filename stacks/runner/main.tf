0a594c8e54e2b3bf0474b99079f08bf3214cef2d
Vivaan <ksubodhgulia@gmail.com>
2023-08-22 11:34:56 +0100

    tst

--- stacks/runner/main.tf
+++ stacks/runner/main.tf
@@ -1,12 +1,11 @@
 resource "aws_instance" "github_runner_instance" {
   for_each = var.github_repos
 
-  ami                    = "ami-02df7c0eecb64fb85"
-  instance_type          = "t4g.small"
-  subnet_id              = "subnet-0140c6f3cbd5d56c7"
-  vpc_security_group_ids = "sg-0189f66c88b7f3708"
-  key_name               = "newacct01"
-  user_data              = file("path/to/userdata.sh")
+  ami           = "ami-02df7c0eecb64fb85"
+  instance_type = "t4g.small"
+  subnet_id     = "subnet-0140c6f3cbd5d56c7"
+  key_name      = "newacct01"
+  user_data     = file("path/to/userdata.sh")
 
   tags = {
     Name = each.key
