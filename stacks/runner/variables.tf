fa39f349f03f574436a7a1c5310aafec6581294e
Vivaan <ksubodhgulia@gmail.com>
2023-08-24 10:59:55 +0100

    Loop2

--- stacks/runner/variables.tf
+++ stacks/runner/variables.tf
@@ -3,8 +3,8 @@ variable "github_runner_repos" {
   type        = map(map(string))
   default = {
     test = {
-      "https://github.com/bbc/sdp-infrastructure"           = "AEHKRMM5NT2Z3ANUMEDHKULE4TRVE"
-      "https://github.com/bbc/sdp-transformations"          = "AEHKRMIDFHPLV7FILZNE2I3E4TR52"
+      "https://github.com/VivaanG1/vpc-network" = "A7MYWEU4EMESTGHNTHDNT5DE443ZO"
+      "https://github.com/VivaanG1/route53"     = "A7MYWET7VD7RCL5TAVA5XSDE4436Q"
     },
     live = {
       // Add more live repositories and tokens here
